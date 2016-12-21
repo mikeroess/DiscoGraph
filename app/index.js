a29a763409a167bd9051f7adc736ba61fc222658
mikeroess <mike.roess@gmail.com>
2016-12-21 17:24:36 -0500

    Write form to allow user to input start and end years + genre.  Store # of releases per year in local storage

---
+++ app/index.js
@@ -0,0 +1,2 @@
+import { GenreQuery } from './discogs_api_calls.js';
+window.GenreQuery = GenreQuery;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./app/index.js",
+  output: {
+    path: path.join('./app'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
