9e3c1c795d5fa9f77e505cc1ee309683c684b528
mikeroess <mike.roess@gmail.com>
2016-12-30 09:30:55 -0500

    Write genrebutton method to make api call on click

--- app/discogs_api_calls.js
+++ app/discogs_api_calls.js
@@ -4,7 +4,8 @@ export const GenreQuery = (genre, year) => {
   return $.ajax({
     method: "GET",
     headers: {
-      "User-Agent": "DiscoGraph/0.1 + https://mikeroess.github.io/DiscoGraph/"
+      "User-Agent": "DiscoGraph/0.1 + https://mikeroess.github.io/DiscoGraph/",
+      "Access-Control-Allow-Headers": *
     },
     url: `https://api.discogs.com/database/search?year=${year}&genre=${genre}&token=${APIToken}&per_page=1&page=1`
   });
