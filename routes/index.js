058a090b7c7eac7cb93876e11a3e7612adc645a0
mikeroess <mike.roess@gmail.com>
2017-01-04 13:17:05 -0500

    Rerender graph when input year changes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,57 @@
 const request = require('request');
 const express = require('express');
+// import each from 'async/each';
 const router = express.Router();
+
+const buildRequests = (genre, startYear, endYear) => {
+  requests = []
+  for (let i = startYear; i <= endYear; i++) {
+    requestVals = {genre, startYear, endYear};
+  requests.push(requestVals)
+  }
+  return requests;
+}
+
+const makeRequest = (requestOpts) => {
+  const genre = requestOpts["genre"];
+  const startYear = requestOpts["startYear"];
+  const endYear = requestOpts["endYear"];
+  const uri = `https://api.discogs.com/database/search?year=${i}&genre=${genre}&token=PjweRsihAfZimRUhZfXdCvpylIVkmjuEWFugyFbr&per_page=1&page=1`;
+  const options = {
+    url: uri,
+    headers: {
+      "User-Agent": "DiscoGraph/0.1 + https://mikeroess.github.io/DiscoGraph/"
+    }
+  };
+  const callback = (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        console.log(results);
+        results[genre][i] = JSON.parse(body)['pagination']['items'];
+        console.log(results);
+        localStorage.setItem("this", "that");
+        return results;
+  } else {
+    console.log(error);
+    console.log(response.statusCode);
+  }
+  };
+  request(options, callback)
+}
+
+
+
+// console.log(options['url']);
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendfile('../public/index.html');
 });
+
+
 
 router.get('/api', function(req, res, next) {
   const genre = req.query["genre"];
@@ -13,38 +59,15 @@ router.get('/api', function(req, res, next) {
   const endYear = req.query["endYear"];
   const results = {};
   results[genre] = {};
-  for (let i = startYear; i <= endYear; i++) {
-    // console.log("start first");
-    // console.log(i);
-    // console.log(genre);
-    const uri = `https://api.discogs.com/database/search?year=${i}&genre=${genre}&token=PjweRsihAfZimRUhZfXdCvpylIVkmjuEWFugyFbr&per_page=1&page=1`;
-    // console.log(uri);
-    const options = {
-      url: uri,
-      headers: {
-        "User-Agent": "DiscoGraph/0.1 + https://mikeroess.github.io/DiscoGraph/"
-      }
-    };
-    // console.log(options['url']);
-    const callback = (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-          console.log(results);
-          results[genre][i] = JSON.parse(body)['pagination']['items'];
-          console.log(results);
-          return results;
-          // console.log()
-          // console.log(body["pagination"]);
-          // console.log(genre);
-          // console.log(i);
-    } else {
-      console.log(error);
-      console.log(response.statusCode);
-    }
-  };
-    const new_results = request(options, callback);
-    console.log("newResults");
-    console.log(new_results);
-  }
+  const apiCalls = buildRequests(genre, startYear, endYear);
+  // for (let i = startYear; i <= endYear; i++) {
+  //   async.each(apiCalls, function(item))
+  //
+  //
+  //   const new_results = request(options, callback);
+  //   console.log("newResults");
+  //   console.log(new_results);
+  // }
   //   $.ajax({
   //     method: "GET",
   //
