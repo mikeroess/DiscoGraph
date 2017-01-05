c9da1828674d639c4b270c82312dd19ff066a907
mikeroess <mike.roess@gmail.com>
2017-01-05 15:28:06 -0500

    bundle js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,252 @@
+const d3 = require('d3');
+// import {genreQuery, subGenreQuery } from './api.js';
 
+
+
+
+const genreQuery = (data) => {
+  return $.ajax({
+    method: "GET",
+    url: 'api',
+    data: data
+  });
+};
+
+const subGenreQuery = (data) => {
+  localStorage.removeItem("subgenre");
+  return $.ajax({
+    method: "GET",
+    url: "api/subGenreQuery",
+    data: data
+  });
+};
+
+const genreButtonClick = function (genre, clicked, startYear, endYear) {
+  writeGraph(localStorage, startYear, endYear);
+  for (let i = startYear; i <= endYear; i++) {
+    let data = {'genre': genre, 'year': i};
+    genreQuery(data).then((response) => {
+      const oldData = JSON.parse(localStorage[genre]);
+      const itemsPerYear = JSON.parse(response["text"])["pagination"]["items"];
+      const year = parseInt(JSON.parse(response["text"])["results"][0]["year"])
+      oldData[year] = itemsPerYear
+      localStorage.setItem(genre, JSON.stringify(oldData))
+      console.log(localStorage)
+      writeGraph(localStorage, startYear, endYear);
+    },
+    (err) => {console.log(err)}
+
+  );
+  }
+};
+
+
+
+const clearChart = () => {
+  $(".graph").remove();
+};
+
+const isButtonClicked = (genre) => {
+  if( genre === "subgenre") return false;
+  const buttonId = genre + "-toggle";
+  return document.getElementById(buttonId).checked;
+};
+
+const margin = {top: 30, right: 20, bottom: 30, left: 50};
+const w = 1050 - margin.left - margin.right;
+const h = 500 - margin.top - margin.bottom;
+
+const xScale = d3.scaleTime()
+              .rangeRound([0, w - margin.left - margin.right]);
+
+const yScale = d3.scaleLinear()
+              .rangeRound([h - margin.bottom - margin.top, 0]);
+
+const line = d3.line()
+      .x(function(d) { return xScale(d[0]) })
+      .y(function(d) { return yScale(d[1]) });
+
+const parseDate = d3.timeParse("%Y");
+
+const formatData = (genre, startYear, endYear) => {
+  const keys = Object.keys(genre).sort();
+  let filteredData = keys.filter((key) => {
+    if (key >= startYear && key <= endYear) {
+      return true
+    } else {
+      return false
+    }
+  })
+  let formattedData = filteredData.map( (key) => {
+    return [parseDate(key), genre[key]] }
+  );
+  return formattedData;
+};
+
+const GenerateLeftAxis = (scale) => {
+  const leftAxis = d3.axisLeft(scale);
+  return leftAxis;
+};
+
+const GenerateBottomAxis = (scale) => {
+const bottomAxis = d3.axisBottom(scale);
+return bottomAxis;
+};
+
+const getMaxRelease = (genres, storage) => {
+  const topReleasesPerGenre = [];
+  genres.forEach( (genre) => {
+    if (genre === "subgenre") return
+  topReleasesPerGenre.push(d3.max(Object.values(storage[genre])));
+  })
+  return d3.max(topReleasesPerGenre);
+};
+
+const writeGraph = (localData, minYear, maxYear) => {
+
+  const genres = Object.keys(localData).filter(
+    (genre) => {
+      if (isButtonClicked(genre)) return genre;
+    }
+  );
+  let globalData = {};
+  genres.forEach( (genre) => {
+    if (localData[genre]) {
+      globalData[genre] = JSON.parse(localData[genre]);
+    }
+  });
+
+
+  const maxNumOfReleases = getMaxRelease(genres, globalData);
+
+  xScale.domain([parseDate(minYear), parseDate(maxYear)]);
+  yScale.domain([0, maxNumOfReleases]);
+
+  const leftAxis = GenerateLeftAxis(yScale);
+  const bottomAxis = GenerateBottomAxis(xScale);
+
+  clearChart();
+
+  let svg = d3.select('#d3Graph').append("svg")
+        .attr("class", "graph")
+        .attr("width", w + margin.left + margin.right)
+        .attr("height", h + margin.top + margin.bottom);
+
+      svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", `translate(${margin.left}, ${margin.bottom})`)
+        .call(leftAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("fill", "#000")
+        .text("Number of Releases");
+
+      svg.append("g")
+      .attr("class", "axis")
+        .attr("transform", `translate(${margin.left}, ${h - margin.bottom})`)
+        .call(bottomAxis);
+
+    genres.forEach( (genre) => {
+      if (genre === 'subgenre') {
+        const subGenreObject = JSON.parse(localStorage[genre])
+        const genreName = Object.keys(subGenreObject)[0];
+        const formattedDataset = formatData(subGenreObject[genreName], minYear, maxYear)
+        if (formattedDataset.length === 0) { return; }
+
+        svg.append("path")
+          .attr("d", line(formattedDataset))
+          .attr("stroke", genreColors[genre])
+          .attr("stroke-width", 2)
+          .attr("fill", "none")
+          .attr("transform", `translate(${margin.left}, ${margin.bottom})`)
+
+        svg.append("text")
+        .attr("transform", "translate(" + (w + 3) + "," + yScale(formattedDataset[formattedDataset.length - 1][1]) + ")")
+        .attr("dy", "0.71em")
+        .attr("class", "genreLabel")
+        .style("fill", "black")
+        .text(genreName);
+
+      } else {
+      const dataset = JSON.parse(localStorage[genre]);
+      const formattedDataset = formatData(dataset, minYear, maxYear);
+      if (formattedDataset.length === 0) { return }
+
+      svg.append("path")
+        .attr("d", line(formattedDataset))
+        .attr("stroke", genreColors[genre])
+        .attr("stroke-width", 2)
+        .attr("fill", "none")
+        .attr("transform", `translate(${margin.left}, ${margin.bottom})`);
+
+      svg.append("text")
+        .attr("transform", "translate(" + (w + 3) + "," + yScale(formattedDataset[formattedDataset.length - 1][1]) + ")")
+        .attr("dy", "0.71em")
+        .attr("class", "genreLabel")
+        .style("fill", genreColors[genre])
+        .text(genre);
+    }}
+  )
+}
+
+
+
+
+const genreColors = {
+  "rock": "#8ce196",
+  "pop": "steelblue",
+  "hip-hop": "purple",
+  "funk-soul": "orange",
+  "electronic": "green",
+  "classical": "brown",
+  "jazz": "red",
+  "subgenre": "black"
+};
+
+const setupLocalStorage = () => {
+  localStorage.removeItem("subgenre");
+  const genres = Object.keys(genreColors);
+  genres.forEach( (genre) => {
+    if (typeof(localStorage[genre]) === "undefined")
+    localStorage.setItem(genre, JSON.stringify({}));
+  })
+};
+
+$(document).ready(() => {
+
+  const rockButton = document.getElementById("rock-toggle");
+  const popButton = document.getElementById("pop-toggle");
+  const hipHopButton = document.getElementById("hip-hop-toggle");
+  const funkSoulButton = document.getElementById("funk-soul-toggle");
+  const electronicButton = document.getElementById("electronic-toggle");
+  const classicalButton = document.getElementById("classical-toggle");
+  const jazzButton = document.getElementById("jazz-toggle");
+  const startYearInput = document.getElementById("startYear");
+
+  rockButton.addEventListener("click", () => genreButtonClick("rock", rockButton.clicked, $('#startYear').val(), $('#endYear').val()), false);
+  popButton.addEventListener("click", () => genreButtonClick("pop", popButton.clicked, $('#startYear').val(), $('#endYear').val()), false);
+  hipHopButton.addEventListener("click", () => genreButtonClick("hip-hop", hipHopButton.clicked, $('#startYear').val(), $('#endYear').val()), false);
+  funkSoulButton.addEventListener("click", () => genreButtonClick("funk-soul", funkSoulButton.clicked, $('#startYear').val(), $('#endYear').val()), false);
+  electronicButton.addEventListener("click", () => genreButtonClick("electronic", electronicButton.clicked, $('#startYear').val(), $('#endYear').val()), false);
+  classicalButton.addEventListener("click", () => genreButtonClick("classical", classicalButton.clicked, $('#startYear').val(), $('#endYear').val()), false);
+  jazzButton.addEventListener("click", () => genreButtonClick("jazz", jazzButton.clicked, $('#startYear').val(), $('#endYear').val()), false);
+  startYearInput.addEventListener("update", () => startYearUpdate($('#startYear').val()));
+
+  setupLocalStorage();
+
+  $("#mainForm").submit( (e) => {
+    e.preventDefault();
+    const data = {'sub_genre': $('#genre').val(), 'startYear': $('#startYear').val(), 'endYear': $('#endYear').val() };
+    writeGraph(localStorage, data["startYear"], data["endYear"]);
+    subGenreQuery(data).then((response) => {
+      const genre = Object.keys(response)[0];
+      const releases = response[Object.keys(response)[0]];
+      const tempObject = {};
+      tempObject[genre] = releases;
+      localStorage.setItem("subgenre", JSON.stringify(tempObject));
+    },
+  (err) => console.log(err));;
+  });
+});
