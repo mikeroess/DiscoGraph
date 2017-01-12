071d5f7aed30bc16508febbd14659eb637f1c954
mikeroess <mike.roess@gmail.com>
2017-01-12 15:05:20 -0500

    Add favicon
    t
    
    nonsense

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -52,10 +52,10 @@
 	
 	var _graph = __webpack_require__(3);
 	
-	var _data_wrangling = __webpack_require__(16);
+	var _data_wrangling = __webpack_require__(5);
 	
 	var d3 = __webpack_require__(4);
-	var RateLimiter = __webpack_require__(13).RateLimiter;
+	var RateLimiter = __webpack_require__(7).RateLimiter;
 	
 	
 	var limiter = new RateLimiter(240, "minute");
@@ -16958,12 +16958,249 @@
 
 
 /***/ },
-/* 5 */,
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	exports.getUnclickedGenres = exports.getClickedGenres = exports.filterFetch = exports.formatData = exports.setupLocalStorage = exports.allGenres = undefined;
+	
+	var _dom_methods = __webpack_require__(2);
+	
+	var _graph = __webpack_require__(3);
+	
+	var allGenres = exports.allGenres = ["rock", "pop", "hip-hop", "funk-soul", "jazz", "classical", "electronic"];
+	
+	var setupLocalStorage = exports.setupLocalStorage = function setupLocalStorage() {
+	  localStorage.removeItem("subgenre");
+	  var genres = Object.keys(_dom_methods.genreColors);
+	  genres.forEach(function (genre) {
+	    if (typeof localStorage[genre] === "undefined") localStorage.setItem(genre, JSON.stringify({}));
+	  });
+	  localStorage.setItem("subgenre", "{}");
+	};
+	
+	var formatData = exports.formatData = function formatData(genre, startYear, endYear) {
+	  var keys = Object.keys(genre).sort();
+	  var filteredData = keys.filter(function (key) {
+	    if (key >= startYear && key <= endYear) {
+	      return true;
+	    }
+	  });
+	  var formattedData = filteredData.map(function (key) {
+	    return [(0, _graph.parseDate)(key), genre[key]];
+	  });
+	  return formattedData;
+	};
+	
+	var filterFetch = exports.filterFetch = function filterFetch(oldEntry, genre, startYear, endYear) {
+	  var missingYears = [];
+	  var start = null;
+	  var end = null;
+	  for (var i = startYear; i < endYear; i++) {
+	    if (start === null) {
+	      if (oldEntry[i] !== undefined) {
+	        continue;
+	      } else {
+	        start = i;
+	      }
+	    } else {
+	      if (oldEntry[i] === undefined) {
+	        continue;
+	      } else {
+	        end = i - 1;
+	        missingYears.push([start, end]);
+	        start = null;
+	      }
+	    }
+	  }
+	  if (start !== null) {
+	    if (oldEntry[endYear] !== undefined) {
+	      missingYears.push([start, endYear - 1]);
+	    } else {
+	      missingYears.push([start, endYear]);
+	    }
+	  } else {
+	    if (oldEntry[endYear] !== undefined) {} else {
+	      missingYears.push([endYear, endYear]);
+	    }
+	  }
+	
+	  return missingYears;
+	};
+	
+	var getClickedGenres = exports.getClickedGenres = function getClickedGenres() {
+	  var clickedGenres = [];
+	  allGenres.forEach(function (genre) {
+	    if ((0, _dom_methods.isButtonClicked)(genre)) {
+	      clickedGenres.push(genre);
+	    }
+	  });
+	  return clickedGenres;
+	};
+	
+	var getUnclickedGenres = exports.getUnclickedGenres = function getUnclickedGenres() {
+	  var clickedGenres = [];
+	  allGenres.forEach(function (genre) {
+	    if (!(0, _dom_methods.isButtonClicked)(genre)) {
+	      clickedGenres.push(genre);
+	    }
+	  });
+	  return clickedGenres;
+	};
+
+/***/ },
 /* 6 */,
-/* 7 */,
-/* 8 */,
-/* 9 */,
-/* 10 */
+/* 7 */
+/***/ function(module, exports, __webpack_require__) {
+
+	
+	exports.RateLimiter = __webpack_require__(8);
+	exports.TokenBucket = __webpack_require__(10);
+
+
+/***/ },
+/* 8 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(process) {var TokenBucket = __webpack_require__(10);
+	
+	/**
+	 * A generic rate limiter. Underneath the hood, this uses a token bucket plus
+	 * an additional check to limit how many tokens we can remove each interval.
+	 * @author John Hurliman <jhurliman@jhurliman.org>
+	 *
+	 * @param {Number} tokensPerInterval Maximum number of tokens that can be
+	 *  removed at any given moment and over the course of one interval.
+	 * @param {String|Number} interval The interval length in milliseconds, or as
+	 *  one of the following strings: 'second', 'minute', 'hour', day'.
+	 * @param {Boolean} fireImmediately Optional. Whether or not the callback
+	 *  will fire immediately when rate limiting is in effect (default is false).
+	 */
+	var RateLimiter = function(tokensPerInterval, interval, fireImmediately) {
+	  this.tokenBucket = new TokenBucket(tokensPerInterval, tokensPerInterval,
+	    interval, null);
+	
+	  // Fill the token bucket to start
+	  this.tokenBucket.content = tokensPerInterval;
+	
+	  this.curIntervalStart = +new Date();
+	  this.tokensThisInterval = 0;
+	  this.fireImmediately = fireImmediately;
+	};
+	
+	RateLimiter.prototype = {
+	  tokenBucket: null,
+	  curIntervalStart: 0,
+	  tokensThisInterval: 0,
+	  fireImmediately: false,
+	
+	  /**
+	   * Remove the requested number of tokens and fire the given callback. If the
+	   * rate limiter contains enough tokens and we haven't spent too many tokens
+	   * in this interval already, this will happen immediately. Otherwise, the
+	   * removal and callback will happen when enough tokens become available.
+	   * @param {Number} count The number of tokens to remove.
+	   * @param {Function} callback(err, remainingTokens)
+	   * @returns {Boolean} True if the callback was fired immediately, otherwise
+	   *  false.
+	   */
+	  removeTokens: function(count, callback) {
+	    // Make sure the request isn't for more than we can handle
+	    if (count > this.tokenBucket.bucketSize) {
+	      process.nextTick(callback.bind(null, 'Requested tokens ' + count +
+	        ' exceeds maximum tokens per interval ' + this.tokenBucket.bucketSize,
+	        null));
+	      return false;
+	    }
+	
+	    var self = this;
+	    var now = Date.now();
+	
+	    // Advance the current interval and reset the current interval token count
+	    // if needed
+	    if (now - this.curIntervalStart >= this.tokenBucket.interval) {
+	      this.curIntervalStart = now;
+	      this.tokensThisInterval = 0;
+	    }
+	
+	    // If we don't have enough tokens left in this interval, wait until the
+	    // next interval
+	    if (count > this.tokenBucket.tokensPerInterval - this.tokensThisInterval) {
+	      if (this.fireImmediately) {
+	        process.nextTick(callback.bind(null, null, -1));
+	      } else {
+	        var waitInterval = Math.ceil(
+	          this.curIntervalStart + this.tokenBucket.interval - now);
+	
+	        setTimeout(function() {
+	          self.tokenBucket.removeTokens(count, afterTokensRemoved);
+	        }, waitInterval);
+	      }
+	      return false;
+	    }
+	
+	    // Remove the requested number of tokens from the token bucket
+	    return this.tokenBucket.removeTokens(count, afterTokensRemoved);
+	
+	    function afterTokensRemoved(err, tokensRemaining) {
+	      if (err) return callback(err, null);
+	
+	      self.tokensThisInterval += count;
+	      callback(null, tokensRemaining);
+	    }
+	  },
+	
+	  /**
+	   * Attempt to remove the requested number of tokens and return immediately.
+	   * If the bucket (and any parent buckets) contains enough tokens and we
+	   * haven't spent too many tokens in this interval already, this will return
+	   * true. Otherwise, false is returned.
+	   * @param {Number} count The number of tokens to remove.
+	   * @param {Boolean} True if the tokens were successfully removed, otherwise
+	   *  false.
+	   */
+	  tryRemoveTokens: function(count) {
+	    // Make sure the request isn't for more than we can handle
+	    if (count > this.tokenBucket.bucketSize)
+	      return false;
+	
+	    var now = Date.now();
+	
+	    // Advance the current interval and reset the current interval token count
+	    // if needed
+	    if (now - this.curIntervalStart >= this.tokenBucket.interval) {
+	      this.curIntervalStart = now;
+	      this.tokensThisInterval = 0;
+	    }
+	
+	    // If we don't have enough tokens left in this interval, return false
+	    if (count > this.tokenBucket.tokensPerInterval - this.tokensThisInterval)
+	      return false;
+	
+	    // Try to remove the requested number of tokens from the token bucket
+	    return this.tokenBucket.tryRemoveTokens(count);
+	  },
+	
+	  /**
+	   * Returns the number of tokens remaining in the TokenBucket.
+	   * @returns {Number} The number of tokens remaining.
+	   */
+	  getTokensRemaining: function () {
+	    this.tokenBucket.drip();
+	    return this.tokenBucket.content;
+	  }
+	};
+	
+	module.exports = RateLimiter;
+	
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(9)))
+
+/***/ },
+/* 9 */
 /***/ function(module, exports) {
 
 	// shim for using process in browser
@@ -17149,155 +17386,7 @@
 
 
 /***/ },
-/* 11 */,
-/* 12 */,
-/* 13 */
-/***/ function(module, exports, __webpack_require__) {
-
-	
-	exports.RateLimiter = __webpack_require__(14);
-	exports.TokenBucket = __webpack_require__(15);
-
-
-/***/ },
-/* 14 */
-/***/ function(module, exports, __webpack_require__) {
-
-	/* WEBPACK VAR INJECTION */(function(process) {var TokenBucket = __webpack_require__(15);
-	
-	/**
-	 * A generic rate limiter. Underneath the hood, this uses a token bucket plus
-	 * an additional check to limit how many tokens we can remove each interval.
-	 * @author John Hurliman <jhurliman@jhurliman.org>
-	 *
-	 * @param {Number} tokensPerInterval Maximum number of tokens that can be
-	 *  removed at any given moment and over the course of one interval.
-	 * @param {String|Number} interval The interval length in milliseconds, or as
-	 *  one of the following strings: 'second', 'minute', 'hour', day'.
-	 * @param {Boolean} fireImmediately Optional. Whether or not the callback
-	 *  will fire immediately when rate limiting is in effect (default is false).
-	 */
-	var RateLimiter = function(tokensPerInterval, interval, fireImmediately) {
-	  this.tokenBucket = new TokenBucket(tokensPerInterval, tokensPerInterval,
-	    interval, null);
-	
-	  // Fill the token bucket to start
-	  this.tokenBucket.content = tokensPerInterval;
-	
-	  this.curIntervalStart = +new Date();
-	  this.tokensThisInterval = 0;
-	  this.fireImmediately = fireImmediately;
-	};
-	
-	RateLimiter.prototype = {
-	  tokenBucket: null,
-	  curIntervalStart: 0,
-	  tokensThisInterval: 0,
-	  fireImmediately: false,
-	
-	  /**
-	   * Remove the requested number of tokens and fire the given callback. If the
-	   * rate limiter contains enough tokens and we haven't spent too many tokens
-	   * in this interval already, this will happen immediately. Otherwise, the
-	   * removal and callback will happen when enough tokens become available.
-	   * @param {Number} count The number of tokens to remove.
-	   * @param {Function} callback(err, remainingTokens)
-	   * @returns {Boolean} True if the callback was fired immediately, otherwise
-	   *  false.
-	   */
-	  removeTokens: function(count, callback) {
-	    // Make sure the request isn't for more than we can handle
-	    if (count > this.tokenBucket.bucketSize) {
-	      process.nextTick(callback.bind(null, 'Requested tokens ' + count +
-	        ' exceeds maximum tokens per interval ' + this.tokenBucket.bucketSize,
-	        null));
-	      return false;
-	    }
-	
-	    var self = this;
-	    var now = Date.now();
-	
-	    // Advance the current interval and reset the current interval token count
-	    // if needed
-	    if (now - this.curIntervalStart >= this.tokenBucket.interval) {
-	      this.curIntervalStart = now;
-	      this.tokensThisInterval = 0;
-	    }
-	
-	    // If we don't have enough tokens left in this interval, wait until the
-	    // next interval
-	    if (count > this.tokenBucket.tokensPerInterval - this.tokensThisInterval) {
-	      if (this.fireImmediately) {
-	        process.nextTick(callback.bind(null, null, -1));
-	      } else {
-	        var waitInterval = Math.ceil(
-	          this.curIntervalStart + this.tokenBucket.interval - now);
-	
-	        setTimeout(function() {
-	          self.tokenBucket.removeTokens(count, afterTokensRemoved);
-	        }, waitInterval);
-	      }
-	      return false;
-	    }
-	
-	    // Remove the requested number of tokens from the token bucket
-	    return this.tokenBucket.removeTokens(count, afterTokensRemoved);
-	
-	    function afterTokensRemoved(err, tokensRemaining) {
-	      if (err) return callback(err, null);
-	
-	      self.tokensThisInterval += count;
-	      callback(null, tokensRemaining);
-	    }
-	  },
-	
-	  /**
-	   * Attempt to remove the requested number of tokens and return immediately.
-	   * If the bucket (and any parent buckets) contains enough tokens and we
-	   * haven't spent too many tokens in this interval already, this will return
-	   * true. Otherwise, false is returned.
-	   * @param {Number} count The number of tokens to remove.
-	   * @param {Boolean} True if the tokens were successfully removed, otherwise
-	   *  false.
-	   */
-	  tryRemoveTokens: function(count) {
-	    // Make sure the request isn't for more than we can handle
-	    if (count > this.tokenBucket.bucketSize)
-	      return false;
-	
-	    var now = Date.now();
-	
-	    // Advance the current interval and reset the current interval token count
-	    // if needed
-	    if (now - this.curIntervalStart >= this.tokenBucket.interval) {
-	      this.curIntervalStart = now;
-	      this.tokensThisInterval = 0;
-	    }
-	
-	    // If we don't have enough tokens left in this interval, return false
-	    if (count > this.tokenBucket.tokensPerInterval - this.tokensThisInterval)
-	      return false;
-	
-	    // Try to remove the requested number of tokens from the token bucket
-	    return this.tokenBucket.tryRemoveTokens(count);
-	  },
-	
-	  /**
-	   * Returns the number of tokens remaining in the TokenBucket.
-	   * @returns {Number} The number of tokens remaining.
-	   */
-	  getTokensRemaining: function () {
-	    this.tokenBucket.drip();
-	    return this.tokenBucket.content;
-	  }
-	};
-	
-	module.exports = RateLimiter;
-	
-	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(10)))
-
-/***/ },
-/* 15 */
+/* 10 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(process) {
@@ -17466,102 +17555,7 @@
 	
 	module.exports = TokenBucket;
 	
-	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(10)))
-
-/***/ },
-/* 16 */
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	exports.getUnclickedGenres = exports.getClickedGenres = exports.filterFetch = exports.formatData = exports.setupLocalStorage = exports.allGenres = undefined;
-	
-	var _dom_methods = __webpack_require__(2);
-	
-	var _graph = __webpack_require__(3);
-	
-	var allGenres = exports.allGenres = ["rock", "pop", "hip-hop", "funk-soul", "jazz", "classical", "electronic"];
-	
-	var setupLocalStorage = exports.setupLocalStorage = function setupLocalStorage() {
-	  localStorage.removeItem("subgenre");
-	  var genres = Object.keys(_dom_methods.genreColors);
-	  genres.forEach(function (genre) {
-	    if (typeof localStorage[genre] === "undefined") localStorage.setItem(genre, JSON.stringify({}));
-	  });
-	  localStorage.setItem("subgenre", "{}");
-	};
-	
-	var formatData = exports.formatData = function formatData(genre, startYear, endYear) {
-	  var keys = Object.keys(genre).sort();
-	  var filteredData = keys.filter(function (key) {
-	    if (key >= startYear && key <= endYear) {
-	      return true;
-	    }
-	  });
-	  var formattedData = filteredData.map(function (key) {
-	    return [(0, _graph.parseDate)(key), genre[key]];
-	  });
-	  return formattedData;
-	};
-	
-	var filterFetch = exports.filterFetch = function filterFetch(oldEntry, genre, startYear, endYear) {
-	  var missingYears = [];
-	  var start = null;
-	  var end = null;
-	  for (var i = startYear; i < endYear; i++) {
-	    if (start === null) {
-	      if (oldEntry[i] !== undefined) {
-	        continue;
-	      } else {
-	        start = i;
-	      }
-	    } else {
-	      if (oldEntry[i] === undefined) {
-	        continue;
-	      } else {
-	        end = i - 1;
-	        missingYears.push([start, end]);
-	        start = null;
-	      }
-	    }
-	  }
-	  if (start !== null) {
-	    if (oldEntry[endYear] !== undefined) {
-	      missingYears.push([start, endYear - 1]);
-	    } else {
-	      missingYears.push([start, endYear]);
-	    }
-	  } else {
-	    if (oldEntry[endYear] !== undefined) {} else {
-	      missingYears.push([endYear, endYear]);
-	    }
-	  }
-	
-	  return missingYears;
-	};
-	
-	var getClickedGenres = exports.getClickedGenres = function getClickedGenres() {
-	  var clickedGenres = [];
-	  allGenres.forEach(function (genre) {
-	    if ((0, _dom_methods.isButtonClicked)(genre)) {
-	      clickedGenres.push(genre);
-	    }
-	  });
-	  return clickedGenres;
-	};
-	
-	var getUnclickedGenres = exports.getUnclickedGenres = function getUnclickedGenres() {
-	  var clickedGenres = [];
-	  allGenres.forEach(function (genre) {
-	    if (!(0, _dom_methods.isButtonClicked)(genre)) {
-	      clickedGenres.push(genre);
-	    }
-	  });
-	  return clickedGenres;
-	};
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(9)))
 
 /***/ }
 /******/ ]);
